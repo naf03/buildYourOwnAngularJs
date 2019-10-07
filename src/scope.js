1821c8f3df6e2aedc86cb293f2ac5d9a046ba088
Alex Fan <alexfan@192-168-1-105.tpgi.com.au>
2019-10-07 20:36:16 +1100

    initial commit

---
+++ src/scope.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function initWatchVal() { }
+
+var _ = require('lodash');
+
+function Scope() {
+    this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+    var watcher = {
+        watchFn: watchFn, 
+        listenerFn: listenerFn, 
+        last: initWatchVal
+    }; 
+    this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+    var self = this; 
+    var newValue, oldValue;
+    _.forEach(this.$$watchers, function(watcher){
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if(newValue !== oldValue){
+            watcher.last = newValue;
+            watcher.listenerFn(newValue, 
+                (oldValue === initWatchVal ? newValue : oldValue), self);
+        }
+    });
+};
+
+module.exports = Scope;
